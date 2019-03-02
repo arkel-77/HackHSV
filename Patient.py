abd0d958328aadd61eb2fd3933b3778a6ba47916
arkelley77 <42101301+arkelley77@users.noreply.github.com>
2019-03-02 14:26:04 -0600

    Add files via upload
    Added deletion methods and smarter add_attribute and add_patient methods

--- Patient.py
+++ Patient.py
@@ -1,12 +1,19 @@
+import os
+
+
 class Patient:
     def __init__(self, SSN: str):
-        self.file = open(SSN + '.txt', 'at')
-        self.file = open(SSN + '.txt')
+        try:
+            self.file_read = open(SSN + '.txt')
+        except:
+            self.file_write = open(SSN + '.txt', 'at')
+            self.file_write.write('SSN:;' + SSN)
+            self.file_read = open(SSN + '.txt')
         self.ssn = SSN
-        self.attributesAndValues = [line.split(':;') for line in self.file.read().split('\n')]
-        self.attributeIndex = [i[0] for i in self.attributesAndValues]
-        self.valuesIndex = [i[1] for i in self.attributesAndValues]
-        self.file = open(SSN + '.txt', 'at')
+        self.attributesAndValues = [line for line in self.file_write.read().split('\n')]
+        self.attributedValues = [line.split(':;') for line in self.attributesAndValues]
+        self.attributeIndex = [i[0] for i in self.attributedValues]
+        self.valuesIndex = [i[1] for i in self.attributedValues]
 
     def get_attribute(self, attribute):
         if attribute in self.attributeIndex:
@@ -14,11 +21,23 @@ class Patient:
         else:
             return None
 
-    def add_attribute(self, attribute, value):
+    def add_or_modify_attribute(self, attribute, value):
         if attribute not in self.attributeIndex:
             self.attributeIndex.append(attribute)
             self.valuesIndex.append(value)
-            self.file.write('\n' + str(attribute) + ':;' + str(value))
+            self.file_write.write('\n' + str(attribute) + ':;' + str(value))
+        else:
+            filedata = self.file_read.read()
+            old_attval = attribute + ':;' + self.valuesIndex[self.attributeIndex.index(attribute)]
+            new_attval = attribute + ':;' + value
+            self.valuesIndex[self.attributeIndex.index(attribute)] = value
+            filedata = filedata.replace(old_attval, new_attval)
+            self.file_write = open(self.ssn + '.txt', 'wt')
+            self.file_write.write(filedata)
+
+    def delete(self):
+        os.remove(self.ssn + '.txt')
+        del self
 
     @property
     def name(self):
@@ -26,6 +45,30 @@ class Patient:
         mn = self.get_attribute('Middle Name')
         ln = self.get_attribute('Last Name')
         if mn is not None:
-            return fn + ' ' + mn + ' ' + ln
+            if fn is not None:
+                if ln is not None:
+                    return fn + ' ' + mn + ' ' + ln
+                else:
+                    return fn + ' ' + mn
+            else:
+                if ln is not None:
+                    return mn + ' ' + ln
+                else:
+                    return mn
         else:
-            return fn + ' ' + ln
+            if fn is not None:
+                if ln is not None:
+                    return fn + ' ' + ln
+                else:
+                    return fn
+            else:
+                if ln is not None:
+                    return ln
+                else:
+                    return ''
+
+    def all_attributes(self):
+        for i in range(len(self.attributeIndex)):
+            yield self.attributeIndex[i] + ':;' + self.valuesIndex[i]
+
+    pass
--- PatientCache.py
+++ PatientCache.py
@@ -5,19 +5,36 @@ class Cache:
     def __init__(self):
         self.file = open('cache.txt', 'at')
         self.file = open('cache.txt')
-        self.patients = [line for line in self.file.read().split('\n')]
-        self.patients = [line.split(':;') for line in self.patients]
-        self.ssns = [patient[1] for patient in self.patients]
-        self.patients = [Patient(ssn) for ssn in self.ssns]
+        patients = [line for line in self.file.read().split('\n')]
+        patients = [line.split(';:') for line in patients]
+        self.patients = []
+        for patient in patients:
+            temp = [attval.split(':;') for attval in patient]
+            try:
+                ssn = temp[0][1]
+                a = Patient(ssn)
+                for attval in temp[1:]:
+                    a.add_or_modify_attribute(attval[0], attval[1])
+                self.patients.append(a)
+            except IndexError:
+                pass
         self.file = open('cache.txt', 'at')
 
-    def add_patient(self, patient):
+    def add_patient(self, patient: Patient):
         self.patients.append(patient)
-        self.file.write(patient.name + ':;' + patient.ssn)
+        self.file.write('\n'+'SSN:;'+patient.ssn)
+        for i in range(len(patient.attributeIndex)):
+            self.file.write(';:'+patient.attributeIndex[i]+':;'+patient.valuesIndex[i])
 
-    def get_patient(self, name=None, SSN=None):
-        if name is None:
-            temp = [patient for patient in self.patients if patient.ssn == SSN]
+    def get_patient(self, SSN: str):
+        temp = [patient for patient in self.patients if patient.ssn == SSN]
+        if len(temp) > 0:
+            return temp[0]
         else:
-            temp = [patient for patient in self.patients if patient.name == name]
-        return temp[0]
+            return None
+
+    def delete(self):
+        self.file = open('cache.txt', 'wt')
+        self.__init__()
+
+    pass
