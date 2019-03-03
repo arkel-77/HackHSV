e6cc428887328c34aebbda28c0128968c5692410
satyoawasistho <48157679+satyoawasistho@users.noreply.github.com>
2019-03-02 18:16:33 -0800

    Add files via upload

--- Editor.py
+++ Editor.py
@@ -1,16 +1,38 @@
 from GUI import *
-
+from tkinter.scrolledtext import*
 
 class Editor(GUI):
-    def __init__(self, patient, dimensions='800x600'):
+
+    def __init__(self, patient, dimensions='800x1400'):
         super().__init__(dimensions, title=patient.name + ' info')
         self.patient = patient
-        self.changed = []
-        self.textboxes = []
-        for i in range(len(patient.attributedValues)):
-            self.textboxes.append(TextBox(column=0,row=i,text=patient.attributedValues[i][0]))
-            self.textboxes.append(TextBox(column=1, row=i, text=patient.attributedValues[i][1]))
-
+        self.addField = Button(self.context,text="Add field",command=self.add_field).grid(row=len(patient.attributeIndex),column=1)
+        self.labels = []
+        self.entries = []
+        for i in range(len(patient.attributeIndex)):
+            self.labels.append(Label(self.context,text=patient.attributeIndex[i]+": ").grid(row=i,column=0))
+            if patient.attributeIndex[i] == 'Schedule' or patient.attributeIndex[i] == 'Current Medicines' or patient.attributeIndex[i] == 'Medical History':
+                self.entries.append(ScrolledText(self.context, height=1).grid(row=i,column=1))
+            else:
+                self.entries.append(Entry(self.context,textvariable=StringVar(self.context,value=patient.valuesIndex[i])).grid(row=i,column=1))
+    def add(self,field,screen):
+        self.patient.assert_attribute(field)
+        self.labels.append(Label(self.context,text=field+": ").grid(row=len(self.patient.attributeIndex)-1,column=0))
+        self.entries.append(Entry(self.context,textvariable=StringVar(self.context,value=self.patient.valuesIndex[len(self.patient.attributeIndex)-1])).grid(row=len(self.patient.attributeIndex)-1,column=1))
+        self.addField.destroy()
+        self.addField = Button(self.context,text="Add field",command=self.add_field).grid(row=len(self.patient.attributeIndex),column=1)
+        screen.destroy()
+    def add_field(self):
+        add_screen = Tk()
+        add_screen.title("New Field")
+        field_label = Label(add_screen, text="Field Name: ")
+        field_label.grid(row=0, column=0)
+        field = Entry(add_screen)
+        field.grid(row=0, column=1)
+        submit = Button(add_screen, text="submit", command=lambda: self.add(field.get(),add_screen))
+        submit.grid(row=0, column=2)
+        add_screen.mainloop()
     def save(self):
-        for i in self.changed:
-            self.patient.change_attribute(i[0], i[1])
+        for i in range(len(patient.attributeIndex)):
+            patient.change_attribute(patient.attributeIndex[i],entries[i].get())
+        
--- main.py
+++ main.py
@@ -13,54 +13,27 @@ add = "I don't see my patient..."
 patientCache = Cache()
 # names list
 names = [unselected, add]
-<<<<<<< HEAD
-#drop options
-def add(ssn):
-   patientCache.add_patient(Patient(ssn))
-def combined_func(ssn,window):
-   add(ssn)
-   window.destroy()
-   edit(ssn)
-def doStuff(var):
-   print(var=="Add a patient (+)")
-   if var=="Add a patient (+)":
-      addScreen=Tk()
-      addScreen.title("New Patient")
-      SSNLabel = Label(addScreen,text="Enter Patient's SSN#: ")
-      SSNLabel.grid(row=0,column=0)
-      SSN=Entry(addScreen)
-      SSN.grid(row=0,column=1)
-      submit=Button(addScreen, text="submit",command=lambda:combined_func(SSN.get(),addScreen))
-      submit.grid(row=0,column=2)
-      addScreen.mainloop()
-      
-selecter = dropDown(mainWindow,names,0,0)
-while(1):
-   if selecter.checkclicked():
-      doStuff(selecter.selected.get())
-   mainWindow.context.update_idletasks()
-   mainWindow.context.update()
-=======
-
+#<<<<<<< HEAD
 
 def edit(patient):
     return Editor(patient)
 
 
 # drop options
-def add(ssn):
-    patientCache.add_patient(ssn)
-
+def add(ssn,add_screen):
+   patientCache.add_patient(Patient(ssn))
+   add_screen.destroy()
+   edit(Patient(ssn))
 
 def do_stuff(var):
-    if var == add:
+    if var == "I don't see my patient...":
         add_screen = Tk()
         add_screen.title("New Patient")
         ssn_label = Label(add_screen, text="Enter Patient's SSN#: ")
         ssn_label.grid(row=0, column=0)
         ssn = Entry(add_screen)
         ssn.grid(row=0, column=1)
-        submit = Button(add_screen, text="submit", command=lambda: add(ssn.get()))
+        submit = Button(add_screen, text="submit", command=lambda: add(ssn.get(),add_screen))
         submit.grid(row=0, column=2)
         add_screen.mainloop()
 
@@ -71,4 +44,4 @@ while True:
         do_stuff(selector.selected.get())
     mainWindow.context.update_idletasks()
     mainWindow.context.update()
->>>>>>> cc890eeda9b9a41899cbbc0598a63941d6e50861
+#>>>>>>> cc890eeda9b9a41899cbbc0598a63941d6e50861
